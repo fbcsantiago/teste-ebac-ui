a0667f5865dbfb0e13befe08302004644ce306ac
fbcsantiago <fernanda.carvalho1985@gmail.com>
2024-05-17 17:44:52 -0300

    faker e variáveis

---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+
+describe('funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    it('deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        //.first()
+        //.last()
+        //.eq(2)
+        .contains('Abominable Hoodie')
+        .click()
+
+       cy.get('#tab-title-description > a').should('contain', 'Descrição')  
+
+    });
+});
