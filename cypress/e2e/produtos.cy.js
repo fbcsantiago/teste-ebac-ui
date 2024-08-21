d565157a7ab04c91c93277e7b470cf2a6316e560
fbcsantiago <fernanda.carvalho1985@gmail.com>
2024-08-21 17:59:25 -0300

    alteração usando massa de dados

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -13,17 +13,34 @@ describe('funcionalidade: Produtos', () => {
             cy.get('#tab-title-description > a').should('contain', 'Descrição')  
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Zeppelin Yoga Pant'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain',produto)
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Zeppelin Yoga Pant')
+        cy.get('.product_title').should('contain','Zeppelin Yoga Pan')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Red', qtd)
+
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
         
     });
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[2].tamanho, 
+                dados[2].cor, 
+                dados[2].quantidade)
+                cy.get('.woocommerce-message').should('contain', dados [2].nomeProduto)
+            })
+        
+            });
 });
