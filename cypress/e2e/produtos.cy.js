ce4f767631cc4be75a1af1ad7d198346e2b56100
fbcsantiago <fernanda.carvalho1985@gmail.com>
2024-08-21 14:56:08 -0300

    alteração para buscar produto pelo produtosPage

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,20 +1,29 @@
 /// <reference types="cypress"/>
+import produtosPage from "../support/page-objects/produtos.page"
+
 
 describe('funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit ('produtos')
+        produtosPage.visitarUrl()
     });
     
-    it('deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Abominable Hoodie')
-        .click()
+    it('Deve selecionar um produto da lista', () => {
+        produtosPage.buscarProdutoLista('Atlas Fitness Tank')
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')  
+    });
 
-       cy.get('#tab-title-description > a').should('contain', 'Descrição')  
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain',produto)
+    });
 
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
